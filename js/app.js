fb55a27ade1c06bdd3589d8791c90bc6363eea5e
Ronalds Breikšs <hamste126@gmail.com>
2015-11-12 17:41:44 +0200

    Refactoring

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 (function () {
 // Ionic Starter App
 
-    var app = angular.module('fiveThreeOne', ['LocalStorageModule', 'fiveThreeOne-planner']);
+    var app = angular.module('fiveThreeOne', ['LocalStorageModule', 'fiveThreeOne-planner', 'fiveThreeOne-history']);
 
 })();
